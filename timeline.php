07f6d5e7779d74102e186e927f142c312e73bc55
LAPTOP-J8V82213\Fukumoto Etsuko <motsu_net@yahoo.co.jp>
2018-11-06 11:47:46 +0800

    タイムライン1行分のHTMLをtimeline_row.phpにお引越し

--- timeline.php
+++ timeline.php
@@ -119,43 +119,12 @@
             <?php endif; ?>
           </form>
         </div>
-        <?php foreach ($feeds as $feed_each ) {?>
-
-
-          <div class="thumbnail">
-
-            <div class="row">
-              <div class="col-xs-1">
-                <img src="user_profile_img/<?php echo $feed_each["profile_img"]; ?>" width="40">
-              </div>
-              <div class="col-xs-11">
-                <?php echo $feed_each["name"]; ?><br>
-                <a href="#" style="color: #7F7F7F;"><?php echo $feed_each["created"]; ?></a>
-              </div>
-            </div>
-            <div class="row feed_content">
-              <div class="col-xs-12" >
-                <span style="font-size: 24px;"><?php echo $feed_each["feed"]; ?></span>
-              </div>
-            </div>
-            <div class="row feed_sub">
-              <div class="col-xs-12">
-                <form method="POST" action="" style="display: inline;">
-                  <input type="hidden" name="feed_id" >
-                  
-                    <input type="hidden" name="like" value="like">
-                    <button type="submit" class="btn btn-default btn-xs"><i class="fa fa-thumbs-up" aria-hidden="true"></i>いいね！</button>
-                </form>
-                <span class="like_count">いいね数 : 100</span>
-                <span class="comment_count">コメント数 : 9</span>
-                <?php if ($_SESSION['id'] == $feed_each["user_id"]) : ?>
-                  <a href="edit.php?feed_id=<?php echo $feed_each['id']; ?>" class="btn btn-success btn-xs">編集</a>
-                  <a onclick="return confirm('本当に消すの？');" href="delete.php?feed_id=<?php echo $feed_each['id']; ?>"class="btn btn-danger btn-xs">削除</a>
-                  <?php endif; ?>
-              </div>
-            </div>
-          </div>
-          <?php } ?>
+        <?php foreach ($feeds as $feed_each ) {
+          include("timeline_row.php");//include=外部ファイルを読み込む。requireと同じような機能
+          //require=読み込んだ外部ファイル内でエラーが発生した場合、、、処理を中断する(用途：DB接続などのエラーが出ると致命的な処理に使用)
+          //include=読み込んだ外部ファイル内でエラーが発生した場合、、、処理を継続する(用途：HTML,CSSなどの表示系に使用＝一部表示にエラーが出ても処理ができる可能性がある)
+          //
+          }?>
         <div aria-label="Page navigation">
           <ul class="pager">
             <li class="previous disabled"><a href="#"><span aria-hidden="true">&larr;</span> Newer</a></li>
